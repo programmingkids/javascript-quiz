2c3298c16494fda6b5421b5a6f4d91baa2e35a76
EC2 Default User <ec2-user@ip-172-31-39-241.ap-northeast-1.compute.internal>
2019-05-25 07:58:03 +0000

    added test01 and test02

---
+++ test02/test04.js
@@ -0,0 +1,23 @@
+/*
+問題4
+以下のようなプログラムがありますが、未完成です。
+実行結果のように表示するために変数「score」に値を代入する必要があります。変数「socre」に値を代入して処理を完成させてください
+
+実行結果
+よくできました
+
+*/
+
+var score =  ;
+
+if( score == 100 ) {
+    console.log('満点です');
+} else if( score >= 90 ) {
+    console.log('よく頑張りました');
+} else if( score >= 80 ) {
+    console.log('よくできました');
+} else if( score >= 70 ) {
+    console.log('あと少し！');
+} else {
+    console.log('もっと頑張ろう');
+}
