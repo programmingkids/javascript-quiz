6f9470cf47dfe000c48b13e5548ee0a0b93d03f8
EC2 Default User <ec2-user@ip-172-31-39-241.ap-northeast-1.compute.internal>
2019-11-11 02:14:14 +0000

    added test06

---
+++ test06/test06.js
@@ -0,0 +1,13 @@
+/*
+実行結果
+18
+*/
+
+function multiply( number1, number2 ) {
+    var answer = number1 * number2;
+    return answer;
+}
+
+var a = 3;
+var b = 6;
+
---
+++ test06/test07.js
@@ -0,0 +1,13 @@
+/*
+実行結果
+25
+*/
+
+function getTriangleArea( base, height ) {
+    var area = base * height / 2;
+    return area;
+}
+
+var b = 10;
+var h = 5;
+
