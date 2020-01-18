a93faa8263fb1855f8b146b30768235348c6c936
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-01-18 05:20:15 +0000

    added test08

---
+++ test08/test07.js
@@ -0,0 +1,13 @@
+/*
+実行結果
+Hi, Tony
+*/
+
+// 関数を修正してはいけません
+// 関数はこのままの形で利用します
+function sayHi( name ) {
+    var message  = 'Hi, ' + name;
+    return message;
+}
+
+
---
+++ test08/test08.js
@@ -0,0 +1,16 @@
+/*
+実行結果
+ArielArielAriel
+*/
+
+// 関数を修正してはいけません
+// 関数はこのままの形で利用します
+function repeatMessage( text, times ) {
+    var message = '';
+    for( var i = 0; i < times; i++) {
+        message += text;
+    }
+    return message;
+}
+
+
